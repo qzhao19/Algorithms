ec024efa98783cb7647bf4fb29ef6f29c195ee59
Zhao Qi <qi.zhao1991@gmail.com>
2019-12-15 23:03:51 +0100

    Create reverse_word.py

---
+++ String/reverse_word.py
@@ -0,0 +1,14 @@
+def reverse_word(strings):
+    """
+    """
+    words = strings.split(' ')
+    i, j = 0, len(words) - 1
+
+    while i < j:
+        words[i], words[j] = words[j], words[i]
+        i += 1
+        j -= 1
+    
+    return ' '.join(words)
+
+reverse_word('That is a boy')
