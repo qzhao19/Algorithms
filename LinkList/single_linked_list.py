f3e29a710bf568ae6ae5024e484556f86f39b7c9
Zhao Qi <qi.zhao1991@gmail.com>
2020-01-15 20:40:40 +0100

    Update single_linked_list.py

--- LinkList/single_linked_list.py
+++ LinkList/single_linked_list.py
@@ -3,62 +3,42 @@ class Node(object):
         self.data = data
         self.next = next
 
+
 class LinkedList(object):
+    """
+    """
     def __init__(self):
-        """
-        """
-        self.head = None
+        # define a head node, default None
+        self._head = None
 
     def is_empty(self):
-        """check if linked list is empty
         """
-        return self.head is None
-    
-    def push(self, data):
-        # create a new node
-        new_node = Node(data)
-        # Make next of new Node as head 
-        new_node.next = self.head
-        self.head = new_node
+        """
+        return self._head = None
 
-    def insert(self, prev_node, data):
-        """Inserts a new node after the given prev_node
+    def append_first(self, data):
+        """Add a new data in the first list
         """
-        # make sure if the given prev_node exists 
-        if prev_node is None: 
-            print('The given previous node must inLinkedList.')
+        new_node = Node(data)
+        new_node.next = self._head
+        self_head = new_node
+
+    def append_last(self, data):
+        """Add a new data node in the last list
+        """
+        new_node = Node(data)
+
+        if self._head is None:
+            self._head = new_node
             return
-        # create a new node
-        new_node = Node(data)
 
-        # new node must point to a node that prev node pointed
-        new_node.next = prev_node.next
-        # change prev_node point to current new node
-        prev_node.next = new_node
+        while self._head.next is not None:
+            self._head = self._head.next
 
-    def append(self, data):
-        """Appends a new node at the end.
-        """
+        self._head.next = new_node
 
-        # create a new node
-        new_node = Node(data)
+    def insert_
 
-        # make sure if a head node is exist, 
-        # then make the new node as head 
-        if self.head is None:
-            self.head = new_node
-            return 
-        last = self.head
-        while last.next is not None:
-            last = last.next
+        
+            
 
-        # last node points to new_node
-        last.next = new_node
-
-    def print_all(self):
-        """
-        """
-        current_node = self.head
-        while current_node:
-            print(current_node.data)
-            current_node = current_node.next
