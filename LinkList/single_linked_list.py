d4fff789011829bc1341ee637004985e9d10a952
Zhao Qi <qi.zhao1991@gmail.com>
2020-01-18 16:15:55 +0100

    Update single_linked_list.py

--- LinkList/single_linked_list.py
+++ LinkList/single_linked_list.py
@@ -6,6 +6,7 @@ class Node(object):
 
 class LinkedList(object):
     """
+
     """
     def __init__(self):
         # define a head node, default None
@@ -14,14 +15,14 @@ class LinkedList(object):
     def is_empty(self):
         """
         """
-        return self._head = None
+        return self._head == None
 
     def append_first(self, data):
         """Add a new data in the first list
         """
         new_node = Node(data)
-        new_node.next = self._head
-        self_head = new_node
+        new_node._next = self._head
+        self._head = new_node
 
     def append_last(self, data):
         """Add a new data node in the last list
@@ -32,10 +33,11 @@ class LinkedList(object):
             self._head = new_node
             return
 
-        while self._head.next is not None:
-            self._head = self._head.next
+        last_node = self._head
+        while last_node._next is not None:
+            last_node = last_node._next
 
-        self._head.next = new_node
+        last_node._next = new_node
 
     def insert_before(self, prev_node, data):
         """Insert a new data node in a specific position which is before a node
@@ -48,26 +50,49 @@ class LinkedList(object):
         new_node = Node(data)
 
         # new node must point to a node that prev node pointed
-        new_node.next = prev_node.next
+        new_node._next = prev_node._next
         # change prev_node point to current new node
-        prev_node.next = new_node
+        prev_node._next = new_node
 
     def find(self, target, next_node, n_nodes):
         """Find the first node from nth succscor node at the target node
 
         """
         while n_nodes>0:
-            curr_node = next_node.next
+            curr_node = next_node._next
             if taget == curr_node.data:
                 return curr_node
             return None
 
 
-    def delete_node(self, target):
-        """
+    def remove(self, target):
+        """Delete a node from linked list
 
         """
-        
-            
+        cur_node = self._head
+
+        if cur_node is None:
+            raise ValueError('Cannot delet')
+        else:
+            if cur_node.data == target:
+                self._head = cur_node._next
+                cur_node = None
+                return 
+
+        while (cur_node is not None) and (cur_node.data != target):
+            # define a prev node
+            prev_node = cur_node
+            cur_node = cur_node._next
+
+        if cur_node is None:
+            raise ValueError('Cannot delete')
+
+        prev_node._next = cur_node._next
+        cur_node = None
 
 
+    def pprint(self):
+        cur_node = self._head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node._next
