5fe5f6b3eba26b152413d22d58d83bf33759038b
qzhao19 <qi.zhao1991@gmail.com>
2020-07-21 09:28:59 +0200

    Update findRepeatNumber.cpp

--- Array/C++/findRepeatNumber.cpp
+++ Array/C++/findRepeatNumber.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int findRepeatNumber(vector<int>& nums) {
-        // sort
+        // method 1: sort
+
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++){
-            int j = i + 1;
-            if (nums[i] == nums[j]){
+        
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i - 1] == nums[i]){
                 return nums[i];
             }
         }
