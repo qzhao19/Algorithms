0559dcbafb0268cd0c62f7120734ddead7e3ef4a
zhaoqi19 <qi.zhao1991@gmail.com>
2020-08-06 22:33:44 +0200

    add

---
+++ Array/C++/mergeinterval.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        // checkout if intervals is empty
+        if (intervals.empty()){
+            return {};
+        }
+        
+        sort(intervals.begin(), intervals.end());
+        
+        vector<vector<int>> ans{intervals[0]};
+        
+        //cout << ans << endl;
+        for (int i = 1; i < intervals.size(); i++){
+            
+            if (intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+                
+            }
+        }
+        return ans;
+            
+    }
+};
