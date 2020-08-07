9c66e1f7dcf9af4190ce856477f94000f6b52286
zhaoqi19 <qi.zhao1991@gmail.com>
2020-08-07 23:28:22 +0200

    add

--- Array/C++/2sum.cpp
+++ Array/C++/2sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-        // You may assume that each input would have exactly one solution.
-        // Example:
-        //construct a hash map to store position of each element
-        // O(n) complexcity
-        
+        if (nums.size() == 0) {
+            return {};
+        }
+
         unordered_map<int, int> hash_map;
-        vector<int> ret;
-        
-        for (int i = 0; i < nums.size(); i++){
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
             hash_map[nums[i]] = i;
         }
-        for (int i = 0; i < nums.size(); i++){
-            const int val = target - nums[i];
-            if (hash_map.find(val) != hash_map.end() && hash_map[val] > i){
-                ret.push_back(i);
-                ret.push_back(hash_map[val]);
+
+        for (int i = 0; i < nums.size(); i++) {
+            int val = target - nums[i];
+            if (hash_map.find(val) != hash_map.end() && hash_map[val] != i) {
+                ans.push_back(i);
+                ans.push_back(hash_map[val]);
                 break;
-            } 
+            }
         }
-        return ret;
-        
+
+        return ans;
     }
 };
