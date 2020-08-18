7d698a3429da3064cee99eb7f31a72d590d8adcc
zhaoqi19 <qi.zhao1991@gmail.com>
2020-08-18 23:28:35 +0200

    add cpp

---
+++ String/C++/isValid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                stk.push(s[i]);
+            }
+            else {
+                if (stk.empty()) return false;
+                if (stk.top() == '(' && s[i] != ')') return false;
+                if (stk.top() == '[' && s[i] != ']') return false;
+                if (stk.top() == '{' && s[i] != '}') return false;
+                stk.pop();
+            }
+        }
+
+        return stk.empty();
+    }
+};
