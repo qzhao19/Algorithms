a8dd2d48895c3ba6eca0a5a210507b5496349602
zhaoqi19 <qi.zhao1991@gmail.com>
2020-10-15 22:39:29 +0800

    modify

--- String/C++/longestPalindrome.cpp
+++ String/C++/longestPalindrome.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
+        if (n == 0) {
+            return "";
+        }
 
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-
-        string ans;
+        string result;
+        vector<vector<int>> dp(n, vector<int>(n));
 
         for (int length = 0; length < n; length++) {
             for (int i = 0; i + length < n; i++) {
                 int j = i + length;
                 if (length == 0) {
-                    dp[i][j] = true;
-                }
+                    dp[i][j] = 1;
+                } 
                 else if (length == 1) {
                     dp[i][j] = (s[i] == s[j]);
                 }
                 else {
-                    dp[i][j] = dp[i+1][j-1] && (s[i] == s[j]);
+                    dp[i][j] = (s[i] == s[j]) && dp[i+1][j-1];
                 }
 
-                if (dp[i][j] && length + 1 > ans.size()) {
-                    ans = s.substr(i, length + 1);
+                if (dp[i][j] && result.size() < length + 1) {
+                    result = s.substr(i, length + 1);
                 }
             }
         }
-
-        return ans;
+        return result;
     }
 };
