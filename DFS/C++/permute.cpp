8a5566cc44ea4d719f3ea6043440d999d6e61c50
zhaoqi19 <qi.zhao1991@gmail.com>
2021-01-17 21:43:32 +0800

    add

--- DFS/C++/permute.cpp
+++ DFS/C++/permute.cpp
@@ -1,37 +1,37 @@
 class Solution {
 private:
-    vector<vector<int>> ans;
+    vector<vector<int>> result;
     vector<bool> status;
     vector<int> path;
-    
 
 public:
-    void dfs(vector<int> &nums, int depth) {
-        // 当前递归层数 == 数组的长度
-        if (nums.size() == depth) {
-            ans.push_back(path);
+
+    void backtrack(vector<int> &nums, int start) {
+        if (nums.size() == start) {
+            result.push_back(path);
             return ;
         }
 
         for (int i = 0; i < nums.size(); i++) {
-            if (status[i]) {
+            if (status[i] == true) {
                 continue;
             }
+
             path.push_back(nums[i]);
             status[i] = true;
-            dfs(nums, depth + 1);
+            backtrack(nums, start + 1);
             path.pop_back();
             status[i] = false;
         }
-
-        
     }
+
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
 
         status.resize(n, false);
-        dfs(nums, 0);
 
-        return ans;
+        backtrack(nums, 0);
+
+        return result;
     }
 };
