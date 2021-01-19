2749a0da989a7b2b1c56948e07aff3998ca55370
zhaoqi19 <qi.zhao1991@gmail.com>
2021-01-19 22:49:59 +0800

    modify

--- String/C++/groupAnagrams.cpp
+++ String/C++/groupAnagrams.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        if (strs.size() == 0) {
+        int n = strs.size();
+        if (n == 0) {
             return {};
         }
+        vector<vector<string>> result;
 
-        vector<vector<string>> ans;
         unordered_map<string, vector<string>> hash_map;
 
-        string cur_str = "";
-        for (int i = 0; i < strs.size(); i++) {
+        string cur_str;
+
+        for (int i = 0; i < n; i++) {
             cur_str = strs[i];
             sort(cur_str.begin(), cur_str.end());
             hash_map[cur_str].push_back(strs[i]);
         }
 
-        for (auto &val : hash_map) {
-            ans.push_back(val.second);
+        for (auto val : hash_map) {
+            result.push_back(val.second);
         }
 
-        return ans;
+        return result;
+
     }
 };
