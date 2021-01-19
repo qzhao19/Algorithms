426cba450fcc29f46d55bf160e45ee5437d5f16f
zhaoqi19 <qi.zhao1991@gmail.com>
2021-01-19 22:49:25 +0800

    modify

--- DFS/C++/solveNQueens.cpp
+++ DFS/C++/solveNQueens.cpp
@@ -1,25 +1,24 @@
 class Solution {
 private:
-    vector<vector<string>> ans;
+    vector<vector<string>> result;
 
 public:
+    
     bool isValid(vector<string> &board, int row, int col) {
-
         int n = board.size();
 
-        // checkout colmuns cases
         for (int i = 0; i < n; i++) {
             if (board[i][col] == 'Q') {
                 return false;
             }
         }
-        // checkout left-top cases
+
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 'Q') {
                 return false;
             }
         }
-        // checkout right-top cases
+
         for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
             if (board[i][j] == 'Q') {
                 return false;
@@ -29,16 +28,16 @@ public:
     }
 
     void backtrack(vector<string> &board, int row) {
-
-        if(board.size() == row) {
-            ans.push_back(board);
+        if (board.size() == row) {
+            result.push_back(board);
             return ;
         }
 
-        for (int col = 0; col < board[row].size(); col++) {
+        for (int col = 0; col < board.size(); col++) {
             if (isValid(board, row, col) == false) {
                 continue;
             }
+
             board[row][col] = 'Q';
             backtrack(board, row + 1);
             board[row][col] = '.';
@@ -50,9 +49,6 @@ public:
 
         backtrack(board, 0);
 
-        return ans;
+        return result;
     }
 };
-
-
-
