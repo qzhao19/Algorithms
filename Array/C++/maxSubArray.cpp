277604f1832b55d3c1759e0e1b44bc6364b78af0
zhaoqi19 <qi.zhao1991@gmail.com>
2021-01-21 22:20:12 +0800

    add

--- Array/C++/maxSubArray.cpp
+++ Array/C++/maxSubArray.cpp
@@ -18,3 +18,46 @@ public:
         return max_sum;
     }
 };
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+
+        int max_sum = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum > max_sum) {
+                    max_sum = sum;
+                }
+            }
+        }
+
+        return max_sum;
+    }
+};
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 0) {
+            return 0;
+        }
+
+        
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        int max_sum = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = max(dp[i-1] + nums[i], nums[i]);
+            max_sum = max(dp[i], max_sum);
+        }
+
+        return max_sum;
+    }
+};
