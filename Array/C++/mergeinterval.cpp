5ac027742887b8ead2dc6e7b0ec6fc3476265211
zhaoqi19 <qi.zhao1991@gmail.com>
2021-01-21 22:34:02 +0800

    modify

--- Array/C++/mergeinterval.cpp
+++ Array/C++/mergeinterval.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        // checkout if intervals is empty
-        if (intervals.empty()){
+        int n = intervals.size();
+        if (n == 0) {
             return {};
         }
-        
+
+        vector<vector<int>> result;
         sort(intervals.begin(), intervals.end());
-        
-        vector<vector<int>> ans{intervals[0]};
-        
-        //cout << ans << endl;
-        for (int i = 1; i < intervals.size(); i++){
-            
-            if (intervals[i][0] > ans.back()[1]){
-                ans.push_back(intervals[i]);
-            }else{
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-                
+
+        result.push_back(intervals[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (result.back()[1] < intervals[i][0]) {
+                result.push_back(intervals[i]);
+            }
+            else {
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
             }
         }
-        return ans;
-            
+
+        return result;
     }
 };
