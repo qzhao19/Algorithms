5cd68fba60ba102e7d9b1f48b73075133032a3a2
zhaoqi19 <qi.zhao1991@gmail.com>
2021-01-28 23:26:52 +0800

    add

--- Array/C++/findMin1.cpp
+++ Array/C++/findMin1.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int ans = 0;
-        int min_val = INT_MAX;
-        for (int i = 0; i < nums.size(); i++){
-            
-            
-            min_val = min(nums[i], min_val);
+        int n = nums.size();
+        int left = 0, right = n - 1;
+
+        if (n == 1) {
+            return nums[0];
         }
-        return min_val;
+
+        if (nums[left] < nums[right]) {
+            return nums[left];
+        }
+        
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+
+            if (nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            }
+
+            if (nums[left] < nums[mid]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+
     }
 };
